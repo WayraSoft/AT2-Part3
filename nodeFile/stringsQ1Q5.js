20991a22076ad0ce9035ec0fc0d7315146ff9536
Wayra <wayrasoft10@gmail.com>
2025-04-09 17:06:39 +1000

    Part 3 - Q3.1
    The following files were created:
    package.json - stringsQ1Q5.js
    Four strings and four operations.

---
+++ nodeFile/stringsQ1Q5.js
@@ -0,0 +1,22 @@
+// File: stringsQ1Q5.js
+// This file shows different strings and operations.
+// The results are verified using node.
+// Author: Carlos
+// Date: 09/04/2025
+
+// Require the file system module.
+const fs = require('node:fs');
+
+// Q3.1 Define four strings and print them in the console.
+
+let myString = "This is a string";
+let anotherString = "   Another string";
+let hello = "Hello there!";
+let myName = "Carlos";
+
+console.log(myString);
+console.log(anotherString);
+console.log(hello);
+console.log(myName);
+
+
