5f9fcb729fa500b47536accf1741ef20fbf331c2
Wayra <wayrasoft10@gmail.com>
2025-04-10 12:13:22 +1000

    Update stringsQ1Q5.js
    New strings created using different methods: concat() - trim() - replace() - split().

--- nodeFile/stringsQ1Q5.js
+++ nodeFile/stringsQ1Q5.js
@@ -34,21 +34,39 @@ let elevenChar = myString.charAt(10);
 console.log(`The 11th character is: ${elevenChar}`);
 
 
-// Q3.3 Use the functions slice and substring to create new strings
+// Q3.3 Use the functions slice and substring to create new strings.
 // Slice "is a" from myString:
 let partSlice = myString.slice(5, 10);
 console.log(`Sliced part: " ${partSlice} "`);
 
-// Substring to get "the" from anotherString"
+// Substring to get "the" from anotherString:
 let partSlice2 = anotherString.substring(6, 9);
 console.log(`Sliced part: " ${partSlice2} "`);
 
 
 // Q3.4 Change the case of the myName string.
-// Change myName string to upper case
+// Change myName string to upper case:
 let myNameUp = myName.toUpperCase();
 console.log(`My name to Upper Case: ${myNameUp}`);
 
-// Change myName string to lower case
+// Change myName string to lower case:
 let myNameLo = myName.toLowerCase();
 console.log(`My name to Upper Case: ${myNameLo}`);
+
+
+// Q3.5 Use various utility methods to create new strings.
+// Use the concat method to join the hello string and myName string:
+let concString = hello.concat(" ", myName);
+console.log(`New concatenated string: ${concString}`);
+
+// Use the trim method to remove the spaces in anotherString:
+let trimAnother = anotherString.trim();
+console.log(`New trimmed string: ${trimAnother}`);
+
+// Use the replace method to change “is a” to an empty space in myString:
+let replMyString = myString.replace("is a", " ");
+console.log(`New replaced string: ${replMyString}`);
+
+// Use the split method on myString using the spaces to break the string:
+let myStringSpl = myString.split(" ");
+console.log(`myString converted: ${myStringSpl}`);
